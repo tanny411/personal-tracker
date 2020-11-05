4aa0c2b18f496636ecc96366962783bc3c130ae0
tanny411 <aysha.kamal7@gmail.com>
2020-11-06 02:49:58 +0600

    feature: Add initial front end component

--- Project-dev.md
+++ Project-dev.md
@@ -3,4 +3,6 @@
 Several things installed in order to create this app are (besides those in the package.json file):
 - node, npm
 - `npm install -g create-react-app` and `create-react-app .` on client folder. This will require node>=10 so break your bones trying to update nodejs. This may help: [Node manual installation](https://github.com/nodesource/distributions/blob/master/README.md#manual-installation).
+- `npm i bootstrap reactstrap uuid react-transition-group` in client folder. Will also be in client package.json. 
+- I used `reactstrap` to load in some of the html components such as navbar.
 
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    // we can make a constructor and bind functions in it
+    // but instead of we use arrow function, we don't need to bind
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         isOpen: false
+    //     }
+    // }
+
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    
+    render() {
+        return (
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">TodoList</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/tanny411">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
+export default AppNavbar;
