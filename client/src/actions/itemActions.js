14bd39836d54c5a5ed97f53bcabf40a1c6b6256b
tanny411 <aysha.kamal7@gmail.com>
2020-11-08 20:14:47 +0600

    feature: Finish app(back-front connection)

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,21 +1,45 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types.js'
+import axios from 'axios';
+import { set } from 'mongoose';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types.js'
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+//this is where thunk comes in, allows us to make async calls with dispatch
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading());
+    axios
+        .get('/api/items')
+        .then(res => 
+            dispatch({
+                type:  GET_ITEMS,
+                payload: res.data
+            })
+        )
 };
 
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        payload: id
-    };
+export const addItem = item => dispatch => {
+    axios
+        .post('/api/items', item)
+        .then(res =>
+            dispatch({
+                type: ADD_ITEM,
+                payload: res.data
+            })   
+        )
+
 };
 
-export const addItem = item => {
+export const deleteItem = id => dispatch => {
+    axios
+        .delete(`/api/items/${id}`)
+        .then(res => 
+            dispatch({
+                type: DELETE_ITEM,
+                payload: id
+            })
+        )
+};
+
+export const setItemsLoading = () => {
     return {
-        type: ADD_ITEM,
-        payload: item
+        type: ITEMS_LOADING
     };
 };
