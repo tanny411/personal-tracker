4da78b0ef9b91aaaef3a1be5e5a9c60e2aaf959a
tanny411 <aysha.kamal7@gmail.com>
2020-11-26 13:20:17 +0600

    feature: Add progress bar component FE

--- client/src/components/user-layout/landing/ProgressComponent.js
+++ client/src/components/user-layout/landing/ProgressComponent.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Progress, Container, Row, Col } from "reactstrap";
+import {Link} from 'react-router-dom';
 
 export default class ProgressComponent extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  progressBarsValues = [
+    { value: 50, color: "success", text: "Money saved", link: "/expenses" },
+    { value: 10, color: "info", text: "Todos Completed", link: "/todo" },
+    { value: 90, color: "warning", text: "Habit Built", link: "#" },
+    { value: 100, color: "danger", text: "Helped Others", link: "#" },
+  ];
+  render() {
+    let progressBarsContent = this.progressBarsValues.map(
+      ({ value, color, text, link }) => (
+        <Link to={link} className="progressbar-link">
+          <Row className="py-3 bg-custom-light">
+            <Col md="3">{text}</Col>
+            <Col md="9" style={{ margin: "auto" }}>
+              <Progress
+                striped
+                value={value}
+                color={color}
+                className="progressbar-styles"
+              >
+                {value}%
+              </Progress>
+            </Col>
+          </Row>
+        </Link>
+      )
+    );
+    return <Container className="my-3">{progressBarsContent}</Container>;
+  }
 }
